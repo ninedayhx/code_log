e1282600efed2b83c00d0e0d2cb51e22098da679
ninedayhx <1170535490@qq.com>
2024-08-24 16:24:55 +0800

    feat:回文链表，先找到链表的中间节点，中间节点以后翻转，然后对比

---
+++ list/234_Palindrome_Linked_List.cpp
@@ -0,0 +1,30 @@
+// tag: list 双指针 stack
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+    }
+};
+
+int main()
+{
+}
