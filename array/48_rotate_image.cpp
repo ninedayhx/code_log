d4651927700e193e700d002220b88039fe9808eb
ninedayhx <1170535490@qq.com>
2024-08-24 14:15:26 +0800

    feat: 48旋转矩阵 模拟

--- array/48_rotate_image.cpp
+++ array/48_rotate_image.cpp
@@ -1,51 +1,70 @@
-#include <iostream>
-#include <vector>
+// tag : array 模拟
+#include <bits/stdc++.h>
 
 using namespace std;
-
-vector<vector<int>> generateMatrix(int n) {
-	vector<vector<int>> v(n, vector<int>(n));
-	if (n == 1) {
-		v[0][0] = 1;
-		return v;
+class Solution
+{
+public:
+	void swap4num(int &a, int &b, int &c, int &d)
+	{
+		int tmp;
+		tmp = a;
+		a = d;
+		d = c;
+		c = b;
+		b = tmp;
 	}
-	int p = 1;
-	int circle = 0;
-	for (int i = n; i > 0; i -= 2) {
-		if(i==1) {
-			v[n/2][n/2] = p;
+	void rotate(vector<vector<int>> &matrix)
+	{
+		// 模拟
+		int n = matrix.size();
+		int circ = n / 2 + n % 2;
+		cout << circ << endl;
+		for (int i = 0; i < circ; i++)
+		{
+			for (int j = i; j < n - i - 1; j++)
+			{
+				swap4num(matrix[i][j],				   // 1 1
+						 matrix[j][n - 1 - i],		   // 1 2
+						 matrix[n - 1 - i][n - 1 - j], // 2 1
+						 matrix[n - 1 - j][i]);		   //
+			}
 		}
-		for (int j = 0; j < i - 1; j++) {
-			v[circle][j + circle] = p;
-			p++;
-		}
-		for (int j = 0; j < i - 1; j++) {
-			v[j + circle][n - 1 - circle] = p;
-			p++;
-		}
-		for (int j = 0; j < i - 1; j++) {
-			v[n - 1 - circle][n - 1 - circle - j] = p;
-			p++;
-		}
-		for (int j = 0; j < i - 1; j++) {
-			v[n - 1 - circle - j][circle] = p;
-			p++;
-		}
-		circle++;
 	}
-	return v;
-}
-
+	static void printMat(vector<vector<int>> &m)
+	{
+		for (auto v : m)
+		{
+			for (auto i : v)
+			{
+				cout << i << ' ';
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+};
 
 int main() {
 	int n;
-	vector<vector<int>> v;
-	while (cin >> n) {
-		v = generateMatrix(n);
-		for (auto i: v) {
-			for (auto j: i)
-				cout << j<<" ";
-			cout << endl;
+	vector<vector<int>> m;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		vector<int> v;
+		while (1)
+		{
+			int x;
+			cin >> x;
+			v.push_back(x);
+			if (cin.get() == '\n')
+				break;
 		}
+		m.push_back(v);
 	}
+	cout << endl;
+	Solution::printMat(m);
+	Solution s;
+	s.rotate(m);
+	Solution::printMat(m);
 }
