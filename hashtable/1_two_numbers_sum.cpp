b34b608c26328517c56894203631298b964259d4
ninedayhx <1170535490@qq.com>
2024-08-27 10:57:56 +0800

    fixed:1 two sum

--- hashtable/1_two_numbers_sum.cpp
+++ hashtable/1_two_numbers_sum.cpp
@@ -23,13 +23,14 @@ vector<int> twoSum(vector<int> &nums, int target) {
 }
 
 vector<int> twoSum_hash(vector<int> &nums, int target) {
-	unordered_map<int, int> table;
-	for(int i = 0; i< nums.size();i++){
-		auto it = table.find(target - nums[i]);
-		if(it!=table.end()){
-			return {it->second, i};
+	unordered_map<int, int> map;
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		if (map.find(target - nums[i]) != map.end())
+		{
+			return {map[target - nums[i]], i};
 		}
-		table[nums[i]] = i;
+		map[nums[i]] = i;
 	}
 }
 
