644fb5f46572389998ca8e4c05ee8cc0b056581a
ninedayhx <1170535490@qq.com>
2024-09-02 13:27:01 +0800

    feat: 更新标准的二分查找找区间和找值的说明

--- array/35_search_insert_position.cpp
+++ array/35_search_insert_position.cpp
@@ -6,57 +6,25 @@ using namespace std;
 int searchInsert(vector<int> &nums, int target)
 {
     int left = 0, right = nums.size() - 1;
-    if (target == nums[right])
-    {
-        return right;
-    }
-    if (target > nums[right])
-    {
-        return right + 1;
-    }
-    if (target <= nums[0])
-    {
-        return 0;
-    }
     int mid = 0;
-
+    int ans = nums.size();
     // 对于这种找区间的题，不能通过使用二分搜索那样
     // 将left 或 mid向左向右缩进一格，缩进一格就可能会漏掉
     // 应该直接将mid作为left或者right
     while (left <= right)
     {
-        mid = (left + right) / 2;
-        if (target == nums[mid])
+        mid = (right + left) >> 1;
+        if (target <= nums[mid])
         {
-            return mid;
+            ans = mid;
+            right = mid - 1;
         }
-        if (right - left == 1 && target < nums[right] && target > nums[left])
+        else
         {
-            return left + 1;
-        }
-        if (right == left)
-        {
-            if (target > nums[left])
-            {
-                return left + 1;
-            }
-            else if (target == nums[left])
-            {
-                return left;
-            }
-            else
-                return left - 1;
-        }
-        if (target > nums[mid])
-        {
-            left = mid;
-        }
-        if (target < nums[mid])
-        {
-            right = mid;
+            left = mid + 1;
         }
     }
-    return -1;
+    return ans;
 }
 
 int main()
--- array/74_search_2d_matrix.cpp
+++ array/74_search_2d_matrix.cpp
@@ -28,50 +28,26 @@ bool binarySearch(vector<int> vec, int t)
 
 bool searchMatrix(vector<vector<int>> &matrix, int target)
 {
-
     int m = matrix.size();
-    int n = matrix[0].size();
-    if (m == 0 || target < matrix[0][0] || target > matrix[m - 1][n - 1])
-        return false;
-    // 找对应行的过程也可变为二分查找
-    // for (int i = 0; i < m; ++i)
-    // {
-    //     if (i < m - 1 && target >= matrix[i][0] && target < matrix[i + 1][0])
-    //     {
-    //         return binarySearch(matrix[i], target);
-    //     }
-    //     if (i == m - 1 && target >= matrix[i][0])
-    //     {
-    //         return binarySearch(matrix[i], target);
-    //     }
-    // }
     int up = 0, down = m - 1;
-    int mid = 0;
+    int ans = -1;
     while (up <= down)
     {
-        mid = (up + down) / 2;
+        int mid = (up + down) >> 1;
         if (target >= matrix[mid][0])
         {
-            if (mid + 1 <= m - 1 && target < matrix[mid + 1][0])
-            {
-                return binarySearch(matrix[mid], target);
-            }
-            if (mid == m - 1)
-            {
-                return binarySearch(matrix[mid], target);
-            }
-            if (target >= matrix[mid + 1][0])
-            {
-                up = mid + 1;
-            }
+            ans = mid;
+            up = mid + 1;
         }
-        if (target < matrix[mid][0])
+        else
         {
             down = mid - 1;
         }
     }
-
-    return false;
+    if (ans >= 0)
+        return binary_search(matrix[ans].begin(), matrix[ans].end(), target);
+    else
+        return false;
 }
 
 int main()
