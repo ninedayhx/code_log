e21d6effce1992367ab2d1b72762d9cdb49264b4
ninedayhx <1170535490@qq.com>
2024-09-22 10:57:43 +0800

    feat: 53 前缀和作法

--- array/53_Maximum_Subarray.cpp
+++ array/53_Maximum_Subarray.cpp
@@ -1,4 +1,4 @@
-// tag:
+// tag: array  动态规划
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -14,6 +14,59 @@ int maxSubArray(vector<int> &nums)
         maxAns = max(maxAns, pre);
     }
     return maxAns;
+}
+
+/**
+ * @brief 前缀和
+ *
+ * @return int
+ */
+int maxSubArray_prevsum(vector<int> &nums)
+{
+    vector<int> presum(nums.size() + 1, 0);
+    presum[0] = 0;
+    for (int i = 1; i <= nums.size(); ++i)
+    {
+        presum[i] = nums[i - 1] + presum[i - 1];
+    }
+    // 此时的最大连续子数组和就变为了找到前缀和数组中最大的那个，和最小的那个，也就是使用贪心算法
+    // 维护最小前缀和,什么都不选时，最小前缀和是0，所以初始为0
+    int min_pre = 0;
+    int max_sum = INT_MIN;
+    // index 从1开始，保证至少会选一个元素
+    for (int i = 1; i < presum.size(); ++i)
+    {
+        // 先按照上一次的最小值去更新最大前缀和，保证至少会选一个元素
+        // 如果先更新最小值，那么就可能当前值就是最小值，然后就会出现不选元素的错误情况
+        max_sum = max(max_sum, presum[i] - min_pre);
+        min_pre = min(min_pre, presum[i]);
+    }
+    return max_sum;
+}
+
+/**
+ * @brief
+ *
+ * @param nums
+ * @return int
+ */
+int maxSubArray_prevsum_opt(vector<int> &nums)
+{
+    // 初始时，什么都不选，前缀和为0
+    int presum = 0;
+    int maxsum = INT_MIN;
+    // 初始时，最小前缀和为0
+    int minpre = 0;
+    for (auto &i : nums)
+    {
+        // 滚动更新前缀和
+        presum += i;
+        // 先使用未更新的最小前缀和求解最大的子数组和
+        // 然后再更新最小前缀和，这样就能保证不会出现没有选元素的情况
+        maxsum = max(maxsum, presum - minpre);
+        minpre = min(minpre, presum);
+    }
+    return maxsum;
 }
 
 int main()
