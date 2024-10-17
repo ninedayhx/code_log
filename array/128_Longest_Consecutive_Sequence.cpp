e7734d8ba18f8e49f26da6156674a014cd3e811b
ninedayhx <1170535490@qq.com>
2024-10-17 16:14:46 +0800

    二刷128，增加了手写的快排函数

--- array/128_Longest_Consecutive_Sequence.cpp
+++ array/128_Longest_Consecutive_Sequence.cpp
@@ -3,40 +3,70 @@
 
 using namespace std;
 
-class Solution
+int longestConsecutive_regular(vector<int> &nums)
 {
-public:
-    int longestConsecutive(vector<int> &nums)
+    sort(nums.begin(), nums.end());
+    int maxlen = 0, len = 0;
+    int tmp = INT32_MAX;
+    for (int i = 0; i < nums.size(); ++i)
     {
-        sort(nums.begin(), nums.end());
-        int maxlen = 0, len = 0;
-        int tmp = INT32_MAX;
-        for (int i = 0; i < nums.size(); ++i)
+        if (nums[i] - 1 == tmp)
+            ++len;
+        else if (nums[i] == tmp)
         {
-            if (nums[i] - 1 == tmp)
-                ++len;
-            else if (nums[i] == tmp)
+            // 处理重复元素
+        }
+        else
+        {
+            maxlen = max(maxlen, len + 1);
+            len = 0;
+        }
+        if (i == nums.size() - 1)
+        {
+            maxlen = max(maxlen, len + 1);
+        }
+        tmp = nums[i];
+    }
+    return maxlen;
+}
+
+void qsort(vector<int> &arr, int l, int r)
+{
+    if (l < r)
+    {
+        int base = arr[l + rand() % (r - l + 1)];
+        int first = l;
+        int last = r;
+        int i = l;
+        while (i <= last)
+        {
+            if (arr[i] == base)
             {
-                // 处理重复元素
+                ++i;
+            }
+            else if (arr[i] < base)
+            {
+                swap(arr[i++], arr[first++]);
             }
             else
             {
-                maxlen = max(maxlen, len + 1);
-                len = 0;
+                swap(arr[i], arr[last--]);
             }
-            if (i == nums.size() - 1)
-            {
-                maxlen = max(maxlen, len + 1);
-            }
-            tmp = nums[i];
         }
-        return maxlen;
+        int part = first;
+        qsort(arr, l, part - 1);
+        qsort(arr, part + 1, r);
     }
-};
+}
 
 int main()
 {
     vector<int> n = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
-    Solution s;
-    cout << s.longestConsecutive(n);
+    qsort(n, 0, n.size() - 1);
+    for (auto &i : n)
+    {
+        cout << i << ' ';
+    }
+    cout << endl;
+    cout << longestConsecutive_regular(n);
 }
